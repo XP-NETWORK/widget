970196591498fb4282980163ee95e683f02be624
ronyXnes <derrarony@gmail.com>
2022-06-28 17:32:06 +0300

    formatting

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,29 +1,29 @@
 import express from 'express'
-import {addUser} from '../controllers/addUser'
-import {getUser} from '../controllers/getUser'
-import {addWidget} from '../controllers/addWidget'
+import { addUser } from '../controllers/addUser'
+import { getUser } from '../controllers/getUser'
+import { addWidget } from '../controllers/addWidget'
 import { getWidget } from '../controllers/getWidget'
 import { verifyUser } from '../middleware/verifyUser'
 import { getWidgetsOfEditor } from '../controllers/getWidgetsOfEditor'
 import { deleteWidget } from '../controllers/deleteWidget'
 import { validateAndStore } from '../controllers/validateAndStore'
 import { updateWidget } from '../controllers/updateWidget'
-import {postTestnetTrx} from "../controllers/postTestnetTrx"
+import { postTestnetTrx } from "../controllers/postTestnetTrx"
 
 const router = express.Router()
 
 //router.get("/",(req: any,res: any)=>{console.log(req.hello);console.log("world");res.send("prints hello world")})
-router.get("/getMyWidget",verifyUser,getWidgetsOfEditor)
-router.get("/getUser",getUser)
-router.get("/getWidget",getWidget)
+router.get("/getMyWidget", verifyUser, getWidgetsOfEditor)
+router.get("/getUser", getUser)
+router.get("/getWidget", getWidget)
 
-router.post("/addUser",addUser)
-router.post("/addWidget",addWidget)
-router.post ("/validateAndStore",validateAndStore)
+router.post("/addUser", addUser)
+router.post("/addWidget", addWidget)
+router.post("/validateAndStore", validateAndStore)
 
-router.delete("/deleteWidget",deleteWidget)
+router.delete("/deleteWidget", deleteWidget)
 
-router.patch("/updateWidget",updateWidget)
+router.patch("/updateWidget", updateWidget)
 
 router.post("testnetTrx", postTestnetTrx)
 export default router
