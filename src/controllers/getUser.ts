f8e2097f41e4e463cbf85ce85323163c4285a65d
ronyXnes <derrarony@gmail.com>
2022-07-06 13:33:04 +0300

    formatting

--- src/controllers/getUser.ts
+++ src/controllers/getUser.ts
@@ -1,13 +1,17 @@
-import USER  from "../models/user"
+import USER from "../models/user"
 
-export const getUser = async(req:any,res:any)=>{
+export const getUser = async (req: any, res: any) => {
     try {
+        if (!req.query.address || !req.query.signature) {
             res.status(400).send("siri: signature or address were not sent")
             return
         }
+        const { address, signature } = req.query
+        const user = await USER.getUser(signature, address)
+
         res.status(200).json(user)
         return
+    } catch (error: any) {
         res.status(500).json({ message: error.toString() });
     }
-    
 }
